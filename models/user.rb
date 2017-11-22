1da544c15e4f5d875b51263d9b0e8a78c1ee1e64
alexv7 <alexvarjabedian7@gmail.com>
2017-11-22 00:23:24 -0500

    auth

--- models/user.rb
+++ models/user.rb
@@ -5,4 +5,15 @@ class User
   field :email, type: String
   field :encrypted_password, type: String
 
+
+
+  # Note that since bcrypt is a 1 way encryption the line below and the line below that are not equivalent
+  # params[:session][:encrypted_password] == BCrypt::Password.new(@user.encrypted_password) # this will give false, its not equivalent to the line below
+  # BCrypt::Password.new(@user.encrypted_password) == params[:session][:encrypted_password] # this will give true and is the same as the one in the method below
+
+  def password_is_correct?(entered_password)
+    BCrypt::Password.new(self.encrypted_password).is_password?(entered_password)
+  end
+
+
 end
--- routes.rb
+++ routes.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'mongoid'
 require 'bcrypt'
 require './models/user'
+require './helpers/app_helpers'
 
 Mongoid.load!(File.expand_path(File.join("config", "mongoid.yml")))
 # no need to create or migrate db, as soon as your first record/document is created, your database will be created and you can see it in the mongo shell (type mongo in cli) using: <show dbs> command
@@ -48,7 +49,7 @@ post "/users" do
  end
 end
 
-
+#--------------------------------------------------------SESSIONS------------------------------------------------------------#
 # sign_in page
 get '/users/sign_in' do
   erb :"sessions/new"
@@ -56,16 +57,15 @@ end
 
 # sign_in authenticate
 post '/users/sign_in' do
+  entered_password = params[:session][:encrypted_password]
   @user = User.find_by(email: params[:session][:email])
-  # Note that since bcrypt is a 1 way encryption the line below and the line below that are not equivalent
-  # params[:session][:encrypted_password] == BCrypt::Password.new(@user.encrypted_password) # this will give false, its not equivalent to the line below
-  if BCrypt::Password.new(@user.encrypted_password) == params[:session][:encrypted_password]
-    puts "Password is correct"
+  if @user && @user.password_is_correct?(entered_password)
     session[:user_id] = @user.id
+    redirect '/'
   else
-    puts "Password is not correct"
+    puts "Incorrect email / password"
+    erb :"/sessions/new"
   end
-  redirect '/'
 end
 
 # sign_out
@@ -73,7 +73,7 @@ delete '/users/sign_out' do
   session[:user_id] = nil
   redirect '/'
 end
-
+#---------------------------------------------------------------------------------------------------------------------------#
 
 # show
 get '/users/:id' do
